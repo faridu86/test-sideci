0fd4b6052945884d4af84144bd5d4a659e2d2748
Farid Bangash <faridu86@gmail.com>
2017-10-21 16:37:31 +0500

    sideci setup

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const one = 1;
+const two = 3;
+
+const three = one + two;
+
+console.log(three);
---
+++ app/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const one = 1;
+const two = 3;
+
+const three = one + two;
+
+console.log(three);
+
+const four = 4
+
+console.log(4);
+
+var result    = one +     two       ;        
+
+let noEndOfline;
+
+var fun = function   (  hello )   {  
+  console.log(hello);
+};
+
+fun(   ' hello how are you '  + '!');
+fun(   ' hello how are you '+'!');
+
+console.log(anc);
